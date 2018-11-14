d535dc27ce5495ddf889aed7ad9d180f8a0b3472
Christopher Bishop <domingobishop@gmail.com>
2018-11-14 20:10:48 +0000

    shop template page

--- inc/functions-widgets.php
+++ inc/functions-widgets.php
@@ -12,6 +12,15 @@ function mh_widgets_init() {
         'after_title'   => '</h3>',
     ) );
     register_sidebar( array(
+        'name' => __( 'Shop sidebar widgets', 'blank-canvas' ),
+        'id' => 'shop-sidebar',
+        'description' => __( 'Widgets in this area will be shown in the shop sidebar.', 'blank-canvas' ),
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3>',
+        'after_title'   => '</h3>',
+    ) );
+    register_sidebar( array(
         'name' => __( 'Footer widget 1', 'theme-slug' ),
         'id' => 'footer-col-1',
         'description' => __( 'Widgets in this area will be shown in the footer.', 'blank-canvas' ),
