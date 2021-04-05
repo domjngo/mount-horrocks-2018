b266b8de6e992a7ef58fa26ca07666a5069a8711
domingobishop <domingobishop@gmail.com>
2021-04-05 15:58:53 +0100

    woocommerce theme support

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ include 'inc/functions-global.php';
 include 'inc/functions-admin.php';
 include 'inc/functions-widgets.php';
 include 'inc/functions-wines.php';
+include 'inc/functions-woo.php';
 include 'inc/functions-temp.php';
 
 add_theme_support( 'post-thumbnails' );
@@ -23,6 +24,24 @@ add_filter( 'previous_posts_link_attributes', 'posts_link_attributes' );
 
 add_shortcode( 'wine', 'wine_shortcode' );
 
+add_action('after_setup_theme', 'woocommerce_support');
+add_action('woocommerce_before_main_content', 'woo_wine_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'woo_wine_wrapper_end', 10);
+
+add_filter('woocommerce_related_products_args', 'woo_wine_remove_related_products', 10);
+add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 );
+add_filter('woocommerce_product_tabs', 'woo_wine_rename_tabs', 98);
+add_filter('woocommerce_product_description_heading', 'woo_wine_change_product_description_tab_heading', 10, 1);
+
+remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
+remove_action('woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30);
+remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0);
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
+remove_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20);
+
 @ini_set( 'upload_max_size' , '6M' );
 @ini_set( 'post_max_size', '6M');
 @ini_set( 'max_execution_time', '100' );
+
---
+++ inc/functions-woo.php
@@ -0,0 +1,43 @@
+<?php
+
+function woocommerce_support()
+{
+    add_theme_support('woocommerce');
+}
+
+function woo_wine_wrapper_start()
+{
+    echo '<main id="main" class="main" role="main">';
+    echo '<div id="content" class="content">';
+    echo '<div class="container"><div class="row"><div class="col-md-12">';
+}
+
+function woo_wine_wrapper_end()
+{
+    echo '</div></div></div></div></main>';
+}
+
+function woo_wine_remove_related_products($args)
+{
+    return array();
+}
+
+function woo_remove_product_tabs( $tabs ) {
+
+    unset( $tabs['reviews'] );
+    unset( $tabs['additional_information'] );
+
+    return $tabs;
+}
+
+function woo_wine_rename_tabs($tabs)
+{
+    $tabs['description']['title'] = __('Tasting notes');
+    return $tabs;
+}
+
+function woo_wine_change_product_description_tab_heading($title)
+{
+    return __('Tasting notes');
+}
+
